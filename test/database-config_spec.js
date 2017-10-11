a3542024239c6dbb7f9b5f5225b2f8b19a465e51
Cedric Michaux <cedric@he8us.be>
2017-10-11 08:18:38 +0200

    Make ESLint pass

--- test/database-config_spec.js
+++ test/database-config_spec.js
@@ -1,13 +1,13 @@
-var config = require('../lib/database-configuration'),
+'use strict';
+
+const config = require('../lib/database-configuration'),
     should = require('should');
 
 
 describe('Database Configuration', function () {
-
     describe('ensureObject', function () {
         it('doesn\'t modify the connection object if it\'s not a string', function () {
-
-            var testObject = {
+            const testObject = {
                 connection: {
                     host: '127.0.0.1',
                     user: 'user',
@@ -17,17 +17,17 @@ describe('Database Configuration', function () {
                 }
             };
 
-            var resultObject = config.ensureObject(testObject);
+            const resultObject = config.ensureObject(testObject);
 
             should(resultObject).be.deepEqual(testObject);
         });
 
         it('converts a mysql connection string to a connection object', function () {
-            var testObject = {
+            const testObject = {
                 connection: 'mysql://user:password@127.0.0.1:12345/ghost'
-            }
+            };
 
-            var resultObject = config.ensureObject(testObject);
+            const resultObject = config.ensureObject(testObject);
 
             should(resultObject.connection).be.deepEqual({
                 host: '127.0.0.1',
@@ -39,11 +39,11 @@ describe('Database Configuration', function () {
         });
 
         it('converts a mysql connection string to a connection object with an optional port', function () {
-            var testObject = {
+            const testObject = {
                 connection: 'mysql://user:password@127.0.0.1/ghost'
-            }
+            };
 
-            var resultObject = config.ensureObject(testObject);
+            const resultObject = config.ensureObject(testObject);
 
             should(resultObject.connection).be.deepEqual({
                 host: '127.0.0.1',
@@ -54,11 +54,11 @@ describe('Database Configuration', function () {
         });
 
         it('converts a mysql connection string with the charset defined to a connection object', function () {
-            var testObject = {
+            const testObject = {
                 connection: 'mysql://user:password@127.0.0.1:12345/ghost?characterEncoding=utf8'
-            }
+            };
 
-            var resultObject = config.ensureObject(testObject);
+            const resultObject = config.ensureObject(testObject);
 
             should(resultObject.connection).be.deepEqual({
                 host: '127.0.0.1',
@@ -71,38 +71,38 @@ describe('Database Configuration', function () {
         });
 
         it('converts a sqlite connection string', function () {
-            var testObject = {
+            const testObject = {
                 connection: 'sqlite:file:/home/user/database.db'
-            }
+            };
 
-            var resultObject = config.ensureObject(testObject);
+            const resultObject = config.ensureObject(testObject);
 
             should(resultObject.connection).be.deepEqual({
-                filename: "/home/user/database.db"
+                filename: '/home/user/database.db'
             });
         });
 
         it('converts a sqlite connection string on windows', function () {
-            var testObject = {
+            const testObject = {
                 connection: 'sqlite:file:///C:/Documents%20and%20Settings/user/Desktop/database.db'
-            }
+            };
 
-            var resultObject = config.ensureObject(testObject);
+            const resultObject = config.ensureObject(testObject);
 
             should(resultObject.connection).be.deepEqual({
-                filename: "/C:/Documents%20and%20Settings/user/Desktop/database.db"
+                filename: '/C:/Documents%20and%20Settings/user/Desktop/database.db'
             });
         });
 
         it('converts a sqlite connection string with the file protocol omitted', function () {
-            var testObject = {
+            const testObject = {
                 connection: 'sqlite:/home/user/database.db'
-            }
+            };
 
-            var resultObject = config.ensureObject(testObject);
+            const resultObject = config.ensureObject(testObject);
 
             should(resultObject.connection).be.deepEqual({
-                filename: "/home/user/database.db"
+                filename: '/home/user/database.db'
             });
         });
     });
