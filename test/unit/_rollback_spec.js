7e2cdb69a2f61aa24265aa6c394942cce72c8d0b
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-09-20 12:39:09 +0100

    Added support for "Irreversible migrations"
    no issue
    
    - some migrations that drop tables which include data will mean that even though the `down` task could re-create the database schema it's still of no use when the data is missing
    - adds support for specifying this in tasks' config with `module.exports.config = {irreversible: true}`
    - if an irreversible migration is included in a rollback task list then an error will be thrown immediately with no tasks being run

--- test/unit/_rollback_spec.js
+++ test/unit/_rollback_spec.js
@@ -177,4 +177,103 @@ describe('Unit: _rollback', function () {
             tasks[3].down.called.should.eql(false);
         });
     });
+
+    it('aborts with irreversible migrations in manual rollback', function () {
+        const knexMigrator = new KnexMigrator({
+            knexMigratorConfig: {
+                database: {},
+                migrationPath: 'location',
+                currentVersion: '3.0'
+            }
+        });
+
+        knexMigrator.connection = sinon.stub().returns({
+            where: sinon.stub().returns({
+                delete: sinon.stub()
+            })
+        });
+
+        const tasks = [
+            {
+                up: sinon.stub(),
+                down: sinon.stub().resolves(),
+                name: '1-something'
+            },
+            {
+                config: {irreversible: true},
+                up: sinon.stub(),
+                down: sinon.stub().resolves(),
+                name: '2-something'
+            },
+            {
+                up: sinon.stub(),
+                down: sinon.stub().resolves(),
+                name: '3-something'
+            }
+        ];
+
+        sinon.stub(utils, 'readTasks').returns(tasks);
+
+        return knexMigrator._rollback({
+            version: '2.31'
+        }).then(function () {
+            true.should.eql(false);
+        }).catch((err) => {
+            should.exist(err);
+            err.errorType.should.eql('IrreversibleMigrationError');
+            tasks[0].down.called.should.eql(false);
+            tasks[1].down.called.should.eql(false);
+            tasks[2].down.called.should.eql(false);
+        });
+    });
+
+    it('aborts with irreversible migrations in automatical failure rollback', function () {
+        const knexMigrator = new KnexMigrator({
+            knexMigratorConfig: {
+                database: {},
+                migrationPath: 'location',
+                currentVersion: '3.0'
+            }
+        });
+
+        knexMigrator.connection = sinon.stub().returns({
+            where: sinon.stub().returns({
+                delete: sinon.stub()
+            })
+        });
+
+        const tasks = [
+            {
+                up: sinon.stub(),
+                down: sinon.stub().resolves(),
+                name: '1-something'
+            },
+            {
+                config: {irreversible: true},
+                up: sinon.stub(),
+                down: sinon.stub().resolves(),
+                name: '2-something'
+            },
+            {
+                up: sinon.stub(),
+                down: sinon.stub().resolves(),
+                name: '3-something'
+            }
+        ];
+
+        sinon.stub(utils, 'readTasks').returns(tasks);
+
+        return knexMigrator._rollback({
+            version: '2.31',
+            task: tasks[2]
+        }).then(function () {
+            true.should.eql(false);
+        }).catch((err) => {
+            should.exist(err);
+            err.errorType.should.eql('IrreversibleMigrationError');
+            tasks[0].down.called.should.eql(false);
+            tasks[1].down.called.should.eql(false);
+            tasks[2].down.called.should.eql(false);
+        });
+    });
 });
